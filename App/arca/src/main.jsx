66ba52a8ebdbc443f156ebffff5d7df5aea5c050
Santiago <socampo430@misena.edu.co>
2023-03-17 18:01:26 -0500

    banner component

--- App/arca/src/main.jsx
+++ App/arca/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Banner from '../components/bannerIndex.jsx';
+import '../assets/css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Banner />
+  </React.StrictMode>
+);
