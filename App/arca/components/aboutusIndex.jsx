30b09a33b5d3af24b88eda21ae498b82d596a495
Santiago <socampo430@misena.edu.co>
2023-03-22 18:07:31 -0500

    About us component

---
+++ App/arca/components/aboutusIndex.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import '../assets/css/index.css'
+import '../assets/css/main.css'
+
+function AboutUs() {
+  return (
+    <a name="Nosotros">
+            <article class="con-about">
+                <span>
+                    <h2>Sobre Nosotros</h2>
+                    <hr></hr>
+                </span>
+                <section>
+                    <h3>Misión</h3>
+                    <p>Reconocemos la diversidad y empoderamos a las personas con discapacidad, a través de la rehabilitación y el deporte, para promover la inclusión económica, social y laboral.</p>
+                </section>
+                <section class="s2">
+                    <h3>Visión</h3>
+                    <p>Trasformar los paradigmas alrededor de la discapacidad, aportando a la construcción de un país más inclusivo para las futuras generaciones.</p>
+                </section>
+            </article>
+        </a>
+  );
+}
+
+export default AboutUs;
--- App/arca/src/main.jsx
+++ App/arca/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Banner from '../components/bannerIndex.jsx';
+import AboutUs from '../components/aboutusIndex';
 import '../assets/css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Banner />
+    <AboutUs />
   </React.StrictMode>
 );
