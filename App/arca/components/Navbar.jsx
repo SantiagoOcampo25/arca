7baae8cc71e5633089dc0b5e935dd52cb2f5a0df
Santiago Ocampo <s.ocampo@globant.com>
2023-06-13 14:10:38 -0500

    Hook Correction

--- App/arca/components/Navbar.jsx
+++ App/arca/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import "../assets/css/components.scss";
 import "../assets/css/main.scss";
